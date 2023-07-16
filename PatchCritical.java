137bb5e824504e20002fdd2d923a608c81e027f4
Draghon0666 <128712922+Draghon0666@users.noreply.github.com>
2023-07-16 20:18:16 +0200

    Update PatchCritical.java

--- PatchCritical.java
+++ PatchCritical.java
@@ -1,4 +1,3 @@
-
 import com.google.common.collect.Multimap;
 import net.minecraft.server.v1_8_R3.*;
 import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
@@ -8,6 +7,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionEffect;
 
 import java.text.DecimalFormat;
 import java.util.Collection;
@@ -23,10 +23,11 @@ public class PatchCritical {
         ItemStack i = p.getItemInHand();
         double c = 1.0;
         if(i != null) c = getAttackDamage(i);//Prendre les dégats de base de l'épée
-        if(p.hasPotionEffect(PotionEffectType.WEAKNESS))
-            c -= 0.5D;//Si il a weakness on ajoute 0.5 coeurs. On passe la weakness en priorité
+        int d = p.getActivePotionEffects().stream().filter(potionEffect -> potionEffect.getType().equals(PotionEffectType.INCREASE_DAMAGE)).map(PotionEffect::getAmplifier).findFirst().orElse(-1) + 1;
+        int dd = p.getActivePotionEffects().stream().filter(potionEffect -> potionEffect.getType().equals(PotionEffectType.WEAKNESS)).map(PotionEffect::getAmplifier).findFirst().orElse(-1) + 1;
+        c -= 0.5D * dd;//Si il a weakness on ajoute 0.5 coeurs. On passe la weakness en priorité
         if(p.hasPotionEffect(PotionEffectType.INCREASE_DAMAGE))//Si il a force on fait * 2.3 pour ses dégats
-           c *= 2.3D;
+            c *= 1.0D + d * 1.3;
         //On va ajouter des dégats si l'item a sharpness
         if(i == null) return c;
         if (i.getEnchantments() == null) return c;//pas d'enchantement donc pas de sharpness
@@ -56,24 +57,23 @@ public class PatchCritical {
         double attackDamage = 1.0;
         UUID uuid = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
         net.minecraft.server.v1_8_R3.ItemStack craftItemStack = CraftItemStack.asNMSCopy(itemStack);
-        if(craftItemStack.getItem() == null) return attackDamage;
+        if(itemStack == null || craftItemStack == null || craftItemStack.getItem() == null) return attackDamage;
         Item item = craftItemStack.getItem();
         if (!(item instanceof ItemSword) && !(item instanceof ItemTool) && !(item instanceof ItemHoe)) return attackDamage;
-            Multimap<String, AttributeModifier> map = item.i();
-            Collection<AttributeModifier> attributes = map.get(GenericAttributes.ATTACK_DAMAGE.getName());
-            if (attributes.isEmpty()) return attackDamage;
-                for (AttributeModifier am : attributes)
-                    if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 0) attackDamage += am.d();
+        Multimap<String, AttributeModifier> map = item.i();
+        Collection<AttributeModifier> attributes = map.get(GenericAttributes.ATTACK_DAMAGE.getName());
+        if (attributes.isEmpty()) return attackDamage;
+        for (AttributeModifier am : attributes)
+            if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 0) attackDamage += am.d();
 
-                double y = 1;
-                for (AttributeModifier am : attributes)
-                    if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 1) y += am.d();
+        double y = 1;
+        for (AttributeModifier am : attributes)
+            if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 1) y += am.d();
 
-                attackDamage *= y;
-                for (AttributeModifier am : attributes)
-                    if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 2)
-                        attackDamage *= (1 + am.d());
-                
+        attackDamage *= y;
+        for (AttributeModifier am : attributes)
+            if (am.a().toString().equalsIgnoreCase(uuid.toString()) && am.c() == 2)
+                attackDamage *= (1 + am.d());
+
         return attackDamage;
     }
-}
